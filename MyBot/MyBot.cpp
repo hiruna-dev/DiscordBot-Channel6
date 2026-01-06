b30ac8651b89e428d3cbd59d8ec0a1ab67ec0776
H1runa <hirunadilmith12@gmail.com>
2026-01-06 11:31:08 +0530

    Can retrieve messages in chunk until the first message

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <dotenv/dotenv.h>
+#include <random>
+#include <iomanip>
 
 using namespace std;
 
@@ -14,33 +16,63 @@ int main() {
 
 	bot.on_ready([&bot](const dpp::ready_t& event) {
 		cout << "Bot initiazlied" << endl;
-		bot.messages_get(1348244337536008193, 0, 1456957892673671244, 0, 30, [](dpp::confirmation_callback_t value) {
-			if (value.is_error()) {
-				cout << "Something went wrong" << endl;
-				cout << value.get_error().human_readable << endl;
+		auto list = make_shared<map<time_t, dpp::message>>(); //ordered map
+
+		//retrieve params
+		int size = 50;
+
+		auto fetch = make_shared<function<void(dpp::snowflake)>>();
+		*fetch = [list, size, fetch, &bot](dpp::snowflake bef) {
+			bot.messages_get(1457638344883437669, 0, bef, 0, size, [list, size, fetch](dpp::confirmation_callback_t value) {
+				if (value.is_error()) {
+					cout << "Something went wrong" << endl;
+					cout << value.get_error().human_readable << endl;
+					return;
 				}
-				
-			auto* msgs = get_if<dpp::message_map>(&value.value);
-			if (!msgs) {
-				cout << "Messages returned empty";
-				return 0;
-			}
 
-			for (dpp::message_map::iterator it = msgs->begin(); it != msgs->end(); it++) {
-				dpp::snowflake id = it->second.id;
-				uint64_t timestamp = (id >> 22) + 1420070400000;
-				time_t seconds = timestamp / 1000;
-				struct tm buf;				
-				localtime_s(&buf, &seconds);			
+				auto* msgs = get_if<dpp::message_map>(&value.value);
+				if (!msgs) {
+					cout << "Messages returned empty" << endl;
+					return;
+				}
 
-				cout << std::put_time(&buf, "%Y-%m-%d %H:%M:%S UTC") << endl;
-			}
+				for (dpp::message_map::iterator it = msgs->begin(); it != msgs->end(); it++) {
+					dpp::snowflake id = it->second.id;
+					uint64_t timestamp = (id >> 22) + 1420070400000;
+					time_t seconds = timestamp / 1000;
+
+					(*list)[seconds] = it->second; //adding time to ordered map
+				}
+
+				cout << "Retrieved: " << msgs->size() << endl;
+
+				//breaking loop
+				if (msgs->size() < size) {
+					cout << "Finished Retrieving" << endl;
+					return;
+				}
+				else {
+					dpp::snowflake id = (*list).begin()->second.id;
+					cout << "Snowflake id: " << id << endl;
+					(*fetch)(id);
+				}
 			});
+		};
 
-	}
-	);
+		// starting the fetch lambda
+		(*fetch)(0);
+	});
 
 	bot.start(dpp::st_wait);
 
 	return 0;
+}
+
+void printMessages(auto& list) {
+	//printing time
+	for (const auto& pair : list) {
+		tm buf;
+		localtime_s(&buf, &pair.first);
+		cout << std::put_time(&buf, "%Y-%m-%d %H:%M:%S UTC") << endl;
+	}
 }
